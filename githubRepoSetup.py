2d23321eef174799b1223beedc8110cc9eafc5fa
z-aditya-pradhan <adityap@zenarate.com>
2025-09-30 02:15:36 +0530

    changes pushed

--- githubRepoSetup.py
+++ githubRepoSetup.py
@@ -293,7 +293,7 @@ def main():
 
     ensure_branches_exist(repo_name, ["qa3" ,"qa", "qa2", "hotfixes", "beta", "perf", "master"])
 
-    add_team_to_repo(repo_name, "devops", permission="push")
+    add_team_to_repo(repo_name, "devops", permission="admin")
     add_team_to_repo(repo_name, "leads", permission="push")
 
     if args.phase == "create_project":
